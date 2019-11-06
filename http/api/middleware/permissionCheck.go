fe5bdb031ec628df8f48b849160db3cad969636f
Xie Qiaoyu <darkseaon@gmail.com>
2019-11-06 17:41:22 +0800

    permission check now use interface{} instead of string

--- http/api/middleware/permissionCheck.go
+++ http/api/middleware/permissionCheck.go
@@ -7,16 +7,16 @@ import (
 
 //CheckPermitFunc 给定一个 context 和一个api 名称,获取request 来源是否有权限访问api
 // 注意，不应该在这个函数中修改 c 的内容, 不会影响主逻辑后和其他中间键的context
-type CheckPermitFunc func(c *gin.Context, apiName string) (pass bool, user interface{}, err error)
+type CheckPermitFunc func(c *gin.Context, permission interface{}) (pass bool, user interface{}, err error)
 
 //PermissionCheck 检查权限的中间键，可以提供多个checkpoints 函数进行权限检查,任意一个检查点未通过都会直接返回403,checkpoint 中的Context是独立的，不会影响实际的request context ，checkpoints 间的context 是共享的
-func PermissionCheck(apiName string, checkpoints ...CheckPermitFunc) gin.HandlerFunc {
+func PermissionCheck(permission interface{}, checkpoints ...CheckPermitFunc) gin.HandlerFunc {
 	// 这个中间键应该放在渲染中间键之前
 	return func(c *gin.Context) {
 		// 在checkpoints 中传递复制避免一些意外情况
 		copyC := c.Copy()
 		for _, check := range checkpoints {
-			pass, user, err := check(copyC, apiName)
+			pass, user, err := check(copyC, permission)
 			if !pass {
 				// 设置http 403 错误
 				api.SetError(c, err, 403)
@@ -29,6 +29,5 @@ func PermissionCheck(apiName string, checkpoints ...CheckPermitFunc) gin.Handler
 			}
 		}
 		c.Next()
-
 	}
 }
